8dadae14cef83c1bc9f316ba21cd79285ab7bd03
TechPandaPro <javaisntcoffee@gmail.com>
2024-07-02 12:06:09 -0700

    Add tray icon, start window placement

--- main.js
+++ main.js
@@ -1,17 +1,45 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Tray } = require("electron");
 const path = require("node:path");
 
-const createWindow = () => {
-  const configWin = new BrowserWindow({
+if (process.platform !== "darwin") throw new Error("Platform not supported");
+
+let tray;
+let configWin;
+let quizWin;
+
+const createWindows = () => {
+  configWin = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, "windows", "config", "preload.js"),
     },
   });
+  configWin.on("close", (e) => {
+    // TODO: figure out window closing (see below in other listener)
+    e.preventDefault();
+    configWin.hide();
+  });
   configWin.loadFile("windows/config/index.html");
 
-  const quizWin = new BrowserWindow({
+  // TODO: window needs to be repositioned whenever it's opened (.setPosition)
+  // TODO: don't hardcode width
+  // const { width: windowWidth } = 800;
+  // const { width: windowWidth } = quizWin.getBounds();
+  // TODO: tray icon needs to be added first
+  // const {
+  //   x: trayX,
+  //   y: trayY,
+  //   width: trayWidth,
+  //   height: trayHeight,
+  // } = tray.getBounds();
+
+  // const windowX = Math.round(trayX + trayWidth / 2 - windowWidth / 2);
+  // const windowY = Math.round(trayY + trayHeight + 10);
+
+  quizWin = new BrowserWindow({
+    // x: windowX,
+    // y: windowY,
     width: 800,
     height: 600,
     webPreferences: {
@@ -20,9 +48,15 @@ const createWindow = () => {
   });
   quizWin.setAlwaysOnTop(true, "floating");
   quizWin.on("close", (e) => {
+    // TODO: figure out how window closing should work for quiz - maybe prevent closing but allow quitting? (use before-quit https://www.electronjs.org/docs/latest/api/app)
     e.preventDefault();
   });
   quizWin.loadFile("windows/quiz/index.html");
+
+  tray = new Tray(path.join(__dirname, "icon.png"));
+  tray.on("click", () => {
+    console.log("Tray clicked");
+  });
 };
 
 app.on("window-all-closed", () => {
@@ -30,9 +64,10 @@ app.on("window-all-closed", () => {
 });
 
 app.whenReady().then(() => {
-  createWindow();
+  createWindows();
 
   app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    if (configWin && !configWin.isVisible()) configWin.show();
+    // if (BrowserWindow.getAllWindows().length === 0) createWindows();
   });
 });
