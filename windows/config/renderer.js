40465c6dcf0dc35b969b40d63ff1e9125a9ef90a
TechPandaPro <javaisntcoffee@gmail.com>
2024-07-02 21:53:01 -0700

    Add saving and retrieving, add question/answer swapping

--- windows/config/renderer.js
+++ windows/config/renderer.js
@@ -1,77 +1,110 @@
-const electronStore = window.electronAPI.electronStore;
+(async () => {
+  const electronStore = window.electronAPI.electronStore;
 
-const questionsBody = document.querySelector("#questions tbody");
-const saveBtn = document.getElementById("saveBtn");
+  const questionsBody = document.querySelector("#questions tbody");
+  const flipQuestionsBtn = document.getElementById("flipQuestionsBtn");
+  const saveBtn = document.getElementById("saveBtn");
 
-addRow();
+  const questions = (await electronStore.get(`questions`)) ?? [];
 
-function addRow() {
-  const newRow = document.createElement("tr");
-  newRow.classList.add("questionInputs");
-  newRow.innerHTML = `
-    <td><div class="customInputWrapper"><input class="customInput questionInput" placeholder="bonjour" /></div></td>
-    <td><div class="customInputWrapper"><input class="customInput answerInput" placeholder="hello" /></div></td>
-    <td><button class="removeBtn" aria-label="Remove"><img src="./close.svg" /></button></td>
-  `;
+  for (const question of questions) addRow(question.question, question.answer);
 
-  const questionInput = newRow.querySelector(".questionInput");
-  const answerInput = newRow.querySelector(".answerInput");
-  const removeBtn = newRow.querySelector(".removeBtn");
+  addRow();
 
-  questionInput.addEventListener("focus", checkFocus);
-  answerInput.addEventListener("focus", checkFocus);
+  function addRow(question, answer) {
+    const newRow = document.createElement("tr");
+    newRow.classList.add("questionInputs");
+    newRow.innerHTML = `
+      <td><div class="customInputWrapper"><input class="customInput questionInput" placeholder="bonjour" /></div></td>
+      <td><div class="customInputWrapper"><input class="customInput answerInput" placeholder="hello" /></div></td>
+      <td><button class="removeBtn" aria-label="Remove"><img src="./close.svg" /></button></td>
+    `;
 
-  questionInput.addEventListener("blur", checkFocus);
-  answerInput.addEventListener("blur", checkFocus);
+    const questionInput = newRow.querySelector(".questionInput");
+    const answerInput = newRow.querySelector(".answerInput");
+    const removeBtn = newRow.querySelector(".removeBtn");
 
-  questionInput.addEventListener("input", checkValues);
-  answerInput.addEventListener("input", checkValues);
+    if (question) questionInput.value = question;
+    if (answer) answerInput.value = answer;
 
-  removeBtn.addEventListener("click", () => newRow.remove());
+    questionInput.addEventListener("focus", checkFocus);
+    answerInput.addEventListener("focus", checkFocus);
 
-  questionsBody.appendChild(newRow);
+    questionInput.addEventListener("blur", checkFocus);
+    answerInput.addEventListener("blur", checkFocus);
 
-  function checkValues() {
-    const allInputs = Array.from(
-      questionsBody.querySelectorAll("tr.questionInputs")
+    questionInput.addEventListener("input", checkValues);
+    answerInput.addEventListener("input", checkValues);
+
+    removeBtn.addEventListener("click", () => newRow.remove());
+
+    questionsBody.appendChild(newRow);
+
+    const newCellBoundingRect = newRow
+      .querySelector("td")
+      .getBoundingClientRect();
+    flipQuestionsBtn.style.left = `${newCellBoundingRect.width}px`;
+    flipQuestionsBtn.style.top = `${newCellBoundingRect.height / 2 + 5}px`;
+
+    function checkValues() {
+      const allInputs = Array.from(
+        questionsBody.querySelectorAll("tr.questionInputs")
+      );
+      const index = allInputs.indexOf(newRow);
+      if (
+        index === allInputs.length - 1 &&
+        (questionInput.value || answerInput.value)
+      )
+        addRow();
+    }
+
+    function checkFocus(e) {
+      const elem = e.target;
+      const elemParent = elem.parentElement;
+      if (document.activeElement === elem) elemParent.dataset.focus = true;
+      else delete elemParent.dataset.focus;
+    }
+  }
+
+  flipQuestionsBtn.addEventListener("click", () => {
+    const questionElems = questionsBody.querySelectorAll("tr.questionInputs");
+    for (const question of questionElems) {
+      const questionElem = question.querySelector(".questionInput");
+      const answerElem = question.querySelector(".answerInput");
+
+      const questionVal = questionElem.value;
+      const answerVal = answerElem.value;
+
+      questionElem.value = answerVal;
+      answerElem.value = questionVal;
+    }
+  });
+
+  saveBtn.addEventListener("click", async () => {
+    const questionElems = questionsBody.querySelectorAll("tr.questionInputs");
+    let questions = Array.from(questionElems).map((question) => ({
+      question: question.querySelector(".questionInput").value,
+      answer: question.querySelector(".answerInput").value,
+      elem: question,
+    }));
+
+    for (let i = 0; i < questions.length - 1; i++) {
+      const question = questions[i];
+      if (!question.question || !question.answer)
+        question.elem.querySelector(".removeBtn").click();
+    }
+
+    questions = questions.filter(
+      (question) => question.question && question.answer
     );
-    const index = allInputs.indexOf(newRow);
-    if (
-      index === allInputs.length - 1 &&
-      (questionInput.value || answerInput.value)
-    )
-      addRow();
-  }
 
-  function checkFocus(e) {
-    const elem = e.target;
-    const elemParent = elem.parentElement;
-    if (document.activeElement === elem) elemParent.dataset.focus = true;
-    else delete elemParent.dataset.focus;
-  }
-}
+    questions = questions.map((question) => ({
+      question: question.question,
+      answer: question.answer,
+    }));
 
-saveBtn.addEventListener("click", () => {
-  const questionElems = questionsBody.querySelectorAll("tr.questionInputs");
-  let questions = Array.from(questionElems).map((question) => ({
-    question: question.querySelector(".questionInput").value,
-    answer: question.querySelector(".answerInput").value,
-    elem: question,
-  }));
+    await electronStore.set(`questions`, questions);
 
-  for (let i = 0; i < questions.length - 1; i++) {
-    const question = questions[i];
-    if (!question.question || !question.answer)
-      question.elem.querySelector(".removeBtn").click();
-  }
-
-  questions = questions.filter(
-    (question) => question.question && question.answer
-  );
-
-  console.log(questions);
-
-  return questions;
-
-  // electronStore.set("");
-});
+    return questions;
+  });
+})();
